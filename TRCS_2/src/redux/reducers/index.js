2cedbda1e2766093d444139a89bb512f119d249d
ptxhien <72014465+ptxhien@users.noreply.github.com>
2022-02-17 11:06:55 +0700

    Source RS (17/2/2022)

--- TRCS_2/src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import ThemeOptions from './ThemeOptions';
-import accountReducer from './account/accountReducer';
-import masterdataReducer from './masterdata/masterdataReducers';
-export default {
-    ThemeOptions,
-    accountReducer,
-    masterdataReducer
-};
